5a85d474fe6c9923b1d55ddc9b1adff7d8d2e085
Marko Poutiainen <regs@sofistes.net>
2023-07-01 13:10:46 +0300

    Fixed jwt_admin_required wrapper. It caused run-time error if jwt was not valid.

--- app/api_jwt.py
+++ app/api_jwt.py
@@ -3,41 +3,11 @@ from flask.wrappers import Response
 from app import jwt
 import json
 from typing import Any, Tuple, NewType, Union, List, Dict, TypedDict
-from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_jwt_extended import get_jwt_identity, jwt_required, verify_jwt_in_request, get_jwt
 from functools import wraps
 from app.route_helpers import new_session
 from app.orm_decl import User
 import json
-
-
-def user_is_admin(id: int) -> bool:
-    session = new_session()
-    user = session.query(User).filter(User.id == id).first()
-
-    if not user:
-        return False
-    return bool(user.is_admin)
-
-
-@jwt.unauthorized_loader
-def unauthorized_callback(callback):
-    resp = make_response(json.dumps({'code': 401,
-                                     'message': 'Ei oikeutta'}), 401)
-    return resp
-
-
-@jwt.expired_token_loader
-def expired_token_callback(callback):
-    resp = make_response(json.dumps({'code': 401,
-                                     'message': 'Istunto on vanhentunut'}), 401)
-    return resp
-
-
-@jwt.invalid_token_loader
-def invalid_token_callback(callback):
-    resp = make_response(json.dumps({'code': 401,
-                                     'message': 'Virheellinen kirjautuminen'}), 401)
-    return resp
 
 
 def jwt_admin_required(f: Any) -> Any:
@@ -49,13 +19,16 @@ def jwt_admin_required(f: Any) -> Any:
 
     If user doesn't have rights, a 401 response is returned.
     """
-    @jwt_required(verify_type=False)
-    @wraps(f)
-    def wrap(*args: Any, **kwargs: Any) -> Any:
-        user_id = get_jwt_identity()
-        if user_is_admin(user_id):
-            return f(*args, **kwargs)
-        else:
-            return make_response(json.dumps({'msg': 'Ei oikeutta toimintoon'}), 401)
-
-    return wrap
+    #@jwt_required(verify_type=False)
+    def wrapper(f: Any) -> Any:
+        @wraps(f)
+        def decorator(*args: Any, **kwargs: Any) -> Any:
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims['is_administrator']:
+                return f(*args, **kwargs)
+            else:
+                return make_response(json.dumps({'msg': 'Ei oikeutta toimintoon'}), 401)
+        return decorator
+    wrapper.__name__ = f.__name__
+    return wrapper
