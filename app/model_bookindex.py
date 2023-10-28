d6c16f0b79fce79e67cec4592feb74df72bdb127
Marko Poutiainen <regs@sofistes.net>
2023-10-28 13:41:51 +0300

    Fixed style issues in model_bookindex.py.

--- app/model_bookindex.py
+++ app/model_bookindex.py
@@ -1,83 +1,128 @@
-from marshmallow import Schema, fields
+# pylint: disable=no-member, too-few-public-methods, too-many-ancestors
+""" SQLAlchemy models for bookindex page. """
+from marshmallow import fields
 
 from app import ma
-from app.orm_decl import (Article, Award, AwardCategory, Awarded, BindingType, Bookseries, Contributor, ContributorRole, Country, Edition,
-                          EditionImage, Genre, Issue, Language, Log, Magazine, Person, PersonLink,
-                          PublicationSize, Publisher, PublisherLink, Pubseries, ShortStory, Tag, User,
-                          Work, Article, StoryType, WorkLink, Format, WorkType)
+from app.orm_decl import (Bookseries, Contributor, ContributorRole, Edition,
+                          EditionImage, Genre, Person,
+                          Publisher, Pubseries, Tag,
+                          Work, WorkType)
+
 
 class BookIndexPubseriesSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Pubseries schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Pubseries
 
+
 class BookIndexPersonSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Person schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Person
 
+
 class BookIndexContributorRoleSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Contributor role schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = ContributorRole
 
+
 class BookIndexEditionImageSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Edition image schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = EditionImage
 
+
 class BookIndexGenreSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Genre schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Genre
     # id = fields.Int(required=True)
     # name = fields.String(required=True)
     # abbr = fields.String()
 
+
 class BookIndexPublisherSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Publisher schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Publisher
 
+
 class BookIndexBookseriesSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Bookseries schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Bookseries
 
+
 class BookIndexTagSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Tag schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Tag
 
+
 class BookIndexWorkContributorSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Work contributor schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Contributor
-    person = fields.Nested(BookIndexPersonSchema(only=('id', 'name', 'alt_name')))
+    person = fields.Nested(BookIndexPersonSchema(
+        only=('id', 'name', 'alt_name')))
     role = fields.Nested(BookIndexContributorRoleSchema)
     description = fields.String()
-    #real_person = fields.Nested(lambda: BookIndexPersonSchema(only=('id', 'name')))
+    # real_person = fields.Nested(
+    # lambda: BookIndexPersonSchema(only=('id', 'name')))
+
 
 class BookIndexWorkEditionSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Edition schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Edition
-
     editors = ma.List(fields.Nested(BookIndexPersonSchema))
     translators = ma.List(fields.Nested(BookIndexPersonSchema))
     contributions = ma.List(fields.Nested(BookIndexWorkContributorSchema))
     images = ma.List(fields.Nested(BookIndexEditionImageSchema))
-    publisher = fields.Nested(lambda: BookIndexPublisherSchema(only=('id', 'name')))
+    publisher = fields.Nested(
+        lambda: BookIndexPublisherSchema(only=('id', 'name')))
+
 
 class BookIndexWorkTypeSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Work type schema. """
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = WorkType
     # id = fields.Int()
     # name = fields.String()
 
-class WorkEditionBriefSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
-    class Meta:
-        model = Edition
 
-    #editors = ma.List(fields.Nested(BookIndexPersonSchema))
-    #translators = ma.List(fields.Nested(PersonBriefSchema))
+class WorkEditionBriefSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Work edition schema. """
+    class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
+        model = Edition
+    # editors = ma.List(fields.Nested(BookIndexPersonSchema))
+    # translators = ma.List(fields.Nested(PersonBriefSchema))
     contributions = ma.List(fields.Nested(BookIndexWorkContributorSchema))
     images = ma.List(fields.Nested(BookIndexEditionImageSchema))
-    publisher = fields.Nested(lambda: BookIndexPublisherSchema(only=('id', 'name')))
-    pubseries = fields.Nested(lambda: BookIndexPubseriesSchema(only=('id', 'name')))
+    publisher = fields.Nested(
+        lambda: BookIndexPublisherSchema(only=('id', 'name')))
+    pubseries = fields.Nested(
+        lambda: BookIndexPubseriesSchema(only=('id', 'name')))
+
 
 class BookIndexSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
+    """ Main schema for BookIndex page. """
+
     class Meta:
+        """ Metadata for SQLAlchemyAutoSchema. """
         model = Work
 
     # id = fields.Number()
@@ -85,13 +130,13 @@ class BookIndexSchema(ma.SQLAlchemyAutoSchema):  # type: ignore
     # orig_title = fields.String()
     # author_str = fields.String()
     contributions = ma.List(fields.Nested(BookIndexWorkContributorSchema))
-    editions = ma.List(fields.Nested(WorkEditionBriefSchema,
-                                     only=['id', 'title', 'pubyear', 'publisher',
-                                           'contributions', 'images', 'version',
-                                           'pubseries',
-                                           'pubseriesnum', 'editionnum']))
+    editions = ma.List(
+        fields.Nested(WorkEditionBriefSchema,
+                      only=['id', 'title', 'pubyear', 'publisher',
+                            'contributions', 'images', 'version',
+                            'pubseries',
+                            'pubseriesnum', 'editionnum']))
     # authors = ma.List(fields.Nested(BookIndexPersonSchema))
     genres = ma.List(fields.Nested(BookIndexGenreSchema))
     bookseries = fields.Nested(BookIndexBookseriesSchema)
     tags = ma.List(fields.Nested(BookIndexTagSchema))
-
