5a1dfc43e2d77e3da81a72da4e74d186e9ba50d3
Marko Poutiainen <regs@sofistes.net>
2023-10-28 13:45:43 +0300

    Fixed style issues in api_jwt.py.

--- app/api_jwt.py
+++ app/api_jwt.py
@@ -1,14 +1,10 @@
-from flask import make_response
-from flask.wrappers import Response
-from app import jwt
+""" JWT authentication stuff. """
 import json
-from typing import Any
-from flask_jwt_extended import verify_jwt_in_request, get_jwt, get_jwt_identity, create_access_token, set_access_cookies
 from functools import wraps
+from typing import Any
+from flask import make_response
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
 from app.orm_decl import User
-import json
-from datetime import datetime, timedelta, timezone
-from app import app
 from app.route_helpers import new_session
 
 # @app.after_request
@@ -18,7 +14,8 @@ from app.route_helpers import new_session
 #         now = datetime.now(timezone.utc)
 #         target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
 #         if target_timestamp > exp_timestamp:
-#             access_token = create_access_token(identity=str(get_jwt_identity()))
+#             access_token = create_access_token(
+#                               identity=str(get_jwt_identity()))
 #             set_access_cookies(response, access_token)
 #         app.logger.info('refresh_existing')
 #         return response
@@ -39,13 +36,13 @@ def jwt_admin_required() -> Any:
         @wraps(f)
         def decorator(*args: Any, **kwargs: Any) -> Any:
             verify_jwt_in_request()
-            id = get_jwt_identity()
+            jwt_id = get_jwt_identity()
             session = new_session()
-            user = session.query(User).filter_by(id=id).first()
+            user = session.query(User).filter_by(id=jwt_id).first()
             if user.is_admin:
                 return f(*args, **kwargs)
             else:
-                return make_response(json.dumps({'msg': 'Ei oikeutta toimintoon'}), 401)
+                return make_response(
+                    json.dumps({'msg': 'Ei oikeutta toimintoon'}), 401)
         return decorator
     return wrapper
-
