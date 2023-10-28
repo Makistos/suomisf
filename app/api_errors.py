5c5d10e110e8e61b3eca3092e7b29cc02efb8389
Marko Poutiainen <regs@sofistes.net>
2023-10-28 13:43:14 +0300

    Fixed style issues in api_errors.py.

--- app/api_errors.py
+++ app/api_errors.py
@@ -1,4 +1,8 @@
+""" API error class. """
+
+
 class APIError(Exception):
+    """ API error class. """
     def __init__(self, message: str, code: int):
         self._message = message
         self._code = code
@@ -6,11 +10,29 @@ class APIError(Exception):
 
     @property
     def message(self) -> str:
+        """
+        Get the message property.
+
+        Returns:
+            str: The value of the message property.
+        """
         return self._message
 
     @property
     def code(self) -> int:
+        """
+        Returns the value of the `code` property.
+
+        :return: An integer representing the value of the `code` property.
+        :rtype: int
+        """
         return self._code
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the object.
+
+        :return: A string representation of the object.
+        :rtype: str
+        """
         return str(self._code) + ': ' + self._message
